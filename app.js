a45ebb42e9e8d1d527afa0e33d167567743198ff
Pavel Shuravin <paulshuravin@gmail.com>
2020-10-22 16:08:51 +0300

    Desperate dance with small add/remove buttons. Finally I've decided completely remove them from UI

--- app.js
+++ app.js
@@ -23,8 +23,22 @@ const saveTable = document.querySelector("#save");
 const table = document.querySelector("table");
 const tableBody = document.querySelector("tbody");
 
+const deleteRow = document.querySelector("#deleterow")
+const addRow = document.querySelector("#addrow")
+
 
 // - Master functions
+// Create new row function
+function createRow (currRow) {
+    // Add new row after current
+    const newRow = tableBody.insertRow(parseInt(currRow.rowIndex));
+    console.log(newRow.rowIndex);
+    // Add new cells into new row
+    for (let i = 0; i < 4; i++) {
+        let newCell = newRow.insertCell(i);
+        newCell.setAttribute("contenteditable", "");
+    }
+}
 
 
 // - Header elements
@@ -81,3 +95,31 @@ copyTable.addEventListener("click", (e) => {
 })
 
 // -- Table body
+// Adjust small buttons to the active row
+// document.addEventListener("click", (e) => {
+//     // Get current row
+//     const currentRow = e.target.parentElement;
+//     // Get small buttons
+//     const smBtn = document.querySelector(".table-button");
+//     if (e.target.parentElement.parentElement.tagName == "TBODY") {
+//         // Append small buttons inside the last cell
+//         currentRow.lastElementChild.style.position = "relative"
+//         currentRow.lastElementChild.appendChild(smBtn)
+//         smBtn.style.position = "absolute"
+//         smBtn.style.transform = "translate(" + currentRow.lastElementChild.offsetWidth + "px, -" + currentRow.lastElementChild.offsetHeight / 2 + "px)";
+//         smBtn.classList.remove("hidden")
+//         // createRow(e.target.parentElement)
+//         console.log(tableBody.children)
+//     } else {
+//     }
+// })
+
+
+
+// Add "create new row" function to its button
+// addRow.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const parentRow = addRow.parentElement.parentElement.parentElement;
+//     console.log(parentRow.rowIndex)
+//     createRow(parentRow);
+// })
